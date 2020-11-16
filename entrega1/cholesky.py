ae62716f7491eb91d54c2aa72d3e42a41fc7751e
dgarciag1 <47048998+dgarciag1@users.noreply.github.com>
2020-11-16 08:24:18 -0500

    Add files via upload

---
+++ entrega1/cholesky.py
@@ -0,0 +1,70 @@
+
+import numpy as np
+import sys
+import cmath 
+
+def cholesky(A):
+    # here is the stages of the method
+    l_stages = {}
+    u_stages = {}
+    aux_A = np.array(A)
+    determinant = np.linalg.det(aux_A) 
+    if determinant == 0:
+        print("Determinant equal to 0")
+        sys.exit(0)
+    # We create the initial "u" matrix with complex type for when we operate roots
+    triangular_top = np.zeros((aux_A.shape[0],aux_A.shape[0]),dtype=np.complex_)
+    # We create the initial "l" matrix with complex type for when we operate roots
+    lower_triangular = np.zeros((aux_A.shape[0],aux_A.shape[0]),dtype=np.complex_)
+    # we add the first stage of the method
+    l_stages[0] = np.array(lower_triangular)
+    u_stages[0] = np.array(triangular_top)
+    # we factor
+    for i in range(A.shape[0]):
+        # var_number is the diagonal number of each column
+        var_number = aux_A[i][i]
+        # if a diagonal number is zero there is no solution for the matrix 
+        if(var_number==0):
+            print(f"There is no solution for this matrix because the diagonal number in A[{i}][{i}] is zero")
+            break
+        result = 0
+        for k in range(len(A)):
+            result += lower_triangular[i][k] * triangular_top[k][i]   
+        # We use the cmath library because when we find the root there 
+        # can be a negative number and the library supports complex numbers 
+        diagonal = cmath.sqrt(A[i][i] - result)
+        lower_triangular[i][i] = diagonal
+        triangular_top[i][i] = lower_triangular[i][i]
+        for k in range(i, len(A)): 
+            if(i != k):
+                # the result will be the operations to find l[i][j] or u[i][j]
+                result = 0
+                for j in range(i):
+                    # we multiply the rows of "l" by the column "i" of "u"
+                    result += (lower_triangular[k][j] * triangular_top[j][i])
+                lower_triangular[k][i] = ((A[k][i] - result)/lower_triangular[i][i])
+        for k in range(i, len(A)):        
+            if (i != k):
+                # the result will be the operations to find l[i][j] or u[i][j]
+                result = 0
+                for j in range(i):
+                    # we multiply the row "i" of "l" by the columns of "u"
+                    result += (lower_triangular[i][j] * triangular_top[j][k])
+                triangular_top[i][k] = ((A[i][k] - result)/lower_triangular[i][i])
+        
+        u_stages[i+1] = np.array(triangular_top)
+        l_stages[i+1] = np.array(lower_triangular)
+    for i in range(len(l_stages)):
+        print(f"Stage {i}:")
+        print("L:")
+        print(l_stages[i])
+        print("U:")
+        print(u_stages[i])
+    l = lower_triangular
+    u = triangular_top
+    return l,u,l_stages,u_stages
+
+np.set_printoptions(precision=7)
+M = [[4, -1, 0, 3],[1, 15.5, 3, 8], [0, -1.3, -4, 1.1], [14, 5, -2, 30]]
+A = np.array(M)
+cholesky(A)
---
+++ entrega1/jacobi.py
@@ -0,0 +1,53 @@
+import numpy as np
+from collections import defaultdict
+
+def jacobi(A,b,x0,tol,Nmax):
+    # here is t matrix, c vector and the spectral radius
+    radius_ct = {}
+    # here is the iterations of the method
+    results = defaultdict(list)
+
+    diagonal = np.diag(np.diag(A))
+    u = np.triu(-np.array(A),1)
+    l = np.tril(-np.array(A),-1)
+    new_matrix = l+u
+    t_results = np.matmul(np.linalg.inv(diagonal),(new_matrix))
+    c_results = np.matmul(np.linalg.inv(diagonal),b)
+    eigenvalues = np.linalg.eig(t_results)
+    # we calculate the largest number of the eigenvalues of t_results
+    radius = max(abs(eigenvalues[0]))
+    radius_ct['T'] = t_results
+    radius_ct['C'] = c_results
+    radius_ct['Spectral Radius'] = radius
+    # we initialize the error to thousand and i counter to zero
+    error = 1000
+    i = 0
+    x_ant = x0
+    results[0] = np.array([0,0,0])
+    while error > tol and i < Nmax: 
+        x_act = np.matmul(t_results,x_ant)+c_results
+        error = np.linalg.norm((x_ant-x_act), ord=2)
+        x_ant = x_act
+        iterations = [i+1,error,x_ant.T]
+        results[i+1] = np.array(iterations)
+        i += 1
+        
+    # we print the results
+    print(f"T:{radius_ct['T']}")
+    print(f"C:{radius_ct['C']}")
+    print(f"Spectral Radius:{radius_ct['Spectral Radius']}")
+    print("|    iter    |     E     |")
+    for j in range(len(results)):
+        print(f"|  {results[j][0]}  |  {results[j][1]}  |  {results[j][2]}  |")
+
+    return radius_ct,results
+
+np.set_printoptions(precision=7)
+M = [[4, -1, 0, 3],[1, 15.5, 3, 8], [0, -1.3, -4, 1.1], [14, 5, -2, 30]]
+A = np.array(M)
+vector_b = [1,1,1,1]
+b = np.array(vector_b)
+x0 = np.zeros((A.shape[0]))
+tol = 0.0000001
+Nmax = 100
+jacobi(A,b,x0,tol,Nmax)
