b8efa66956478c87bf386034aa04ca23fa64950d
dgarciag1 <47048998+dgarciag1@users.noreply.github.com>
2020-11-19 10:44:59 -0500

    Add files via upload

---
+++ entrega1/lagrange.py
@@ -0,0 +1,38 @@
+import numpy as np
+import sympy as sm
+
+def lagrange(table):
+    x_vector = np.array(table[0])
+    y_vector = np.array(table[1])
+    results = {}
+    #we create the unknown of the equation
+    x = sm.symbols('x')
+    n = len(x_vector)
+    final_equation = []
+    polynomial = 0
+    for i in range(0,n):
+        #numerator of our expression
+        num = 1
+        #denominator of our expression
+        den = 1
+        for j in range(0,n):
+            if j != i:
+                num = num*(x-x_vector[j])
+                den = den*(x_vector[i]-x_vector[j])
+        # Lagrange interpolating polynomials
+        interpolant = num/den
+        interpolant = interpolant.expand()
+        results[i] = interpolant
+        # here we put each equation
+        equation = num*y_vector[i]/den
+        equation = equation.expand()
+        # we add each equation to obtain the final polynomial
+        polynomial += equation
+
+    results['polynomial'] = polynomial
+    print(results)
+    return results
+
+np.set_printoptions(precision=7)
+table = [[-1, 0, 3, 4],[15.5, 3, 8, 1]]
+lagrange(table)
